a64569566d113ac7659ca1377b7b06e6389d96d0
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-10-15 17:03:55 +0200

    added webpack configuration bcs of failed builds

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,15 @@
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === 'build-html' || stage === 'develop-html') {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /react-p5/,
+              use: 'null-loader'
+            }
+          ]
+        }
+      });
+    }
+  };
