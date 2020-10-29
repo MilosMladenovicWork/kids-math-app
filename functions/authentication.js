e5c0e97aa8753d24940137055d5f8ff4b1bc0cd2
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-10-29 15:08:13 +0100

    removed code from db connected body for run testing

--- functions/authentication.js
+++ functions/authentication.js
@@ -14,47 +14,12 @@ module.exports.handler = function (event, context, callback) {
     return username == 'admin' && password == 'admin';
   };
 
-  let authenticated;
   const data = event.queryStringParameters;
   mongoose.connect(MONGODB_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => {
-    console.log('connected database');
-    const db = mongoose.connection;
-    db.on('error', function (error) {
-      console.log(error);
-      let errorBody = JSON.stringify({
-        status: 'error',
-        message: 'Došlo je do problema na našim serverima, pokušajte kasnije!'
-      });
-      callback(null, {
-        statusCode: 500,
-        body: errorBody
-      });
-    });
-    db.once('open', function () {
-      console.log('hey');
-      const userSchema = new mongoose.Schema({
-        username: String,
-        pass: String
-      });
-      const User = new mongoose.model('User', userSchema);
-      authenticated = checkCredentials(data.username, data.pass);
-      let authenticationSuccessBody = JSON.stringify({
-        status: 'success',
-        message: 'Uspešno ste prijavljeni!'
-      });
-      let authenticationErrorBody = JSON.stringify({
-        status: 'error',
-        message: 'Korisničko ime ili šifra su netačni, pokušajte ponovo!'
-      });
-      callback(null, {
-        statusCode: 200,
-        // http status code
-        body: authenticated ? authenticationSuccessBody : authenticationErrorBody
-      });
-    });
+    console.log('database connected');
   }).catch(e => {
     console.log(e);
     let errorBody = JSON.stringify({
@@ -64,6 +29,40 @@ module.exports.handler = function (event, context, callback) {
     callback(null, {
       statusCode: 500,
       body: errorBody
+    });
+  });
+  const db = mongoose.connection; // db.on('error', function(error){
+  //   console.log(error)
+  //   let errorBody = JSON.stringify({
+  //     status: 'error',
+  //     message: 'Došlo je do problema na našim serverima, pokušajte kasnije!'
+  //   });
+  //   callback(null, {
+  //     statusCode: 500,
+  //     body: errorBody
+  //   })
+  // });
+
+  db.once('open', function () {
+    console.log('hey'); // const userSchema = new mongoose.Schema({
+    //   username: String,
+    //   pass: String
+    // });
+    // const User = new mongoose.model('User', userSchema);
+    // let authenticated = checkCredentials(data.username, data.pass);
+    // let authenticationSuccessBody = JSON.stringify({
+    //   status: 'success',
+    //   message: 'Uspešno ste prijavljeni!'
+    // });
+    // let authenticationErrorBody = JSON.stringify({
+    //   status: 'error',
+    //   message: 'Korisničko ime ili šifra su netačni, pokušajte ponovo!'
+    // });
+
+    callback(null, {
+      statusCode: 200,
+      // http status code
+      body: authenticated ? authenticationSuccessBody : authenticationErrorBody
     });
   });
 };
--- src/lambda/authentication.js
+++ src/lambda/authentication.js
@@ -18,42 +18,9 @@ module.exports.handler = function (event, context, callback) {
     mongoose.connect(MONGODB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    }).then(() => {
-      console.log('connected database')
-      const db = mongoose.connection;
-      db.on('error', function(error){
-        console.log(error)
-        let errorBody = JSON.stringify({
-          status: 'error',
-          message: 'Došlo je do problema na našim serverima, pokušajte kasnije!'
-        });
-        callback(null, {
-          statusCode: 500,
-          body: errorBody
-        })
-      });
-      db.once('open', function () {
-        console.log('hey');
-        // const userSchema = new mongoose.Schema({
-        //   username: String,
-        //   pass: String
-        // });
-        // const User = new mongoose.model('User', userSchema);
-        // let authenticated = checkCredentials(data.username, data.pass);
-        // let authenticationSuccessBody = JSON.stringify({
-        //   status: 'success',
-        //   message: 'Uspešno ste prijavljeni!'
-        // });
-        // let authenticationErrorBody = JSON.stringify({
-        //   status: 'error',
-        //   message: 'Korisničko ime ili šifra su netačni, pokušajte ponovo!'
-        // });
-        callback(null, {
-          statusCode: 200,
-          // http status code
-          body: authenticated ? authenticationSuccessBody : authenticationErrorBody
-        })
-      });
+    })
+    .then(() => {
+        console.log('database connected')
     }).catch(e => {
         console.log(e)
         let errorBody = JSON.stringify({
@@ -66,4 +33,38 @@ module.exports.handler = function (event, context, callback) {
         })
     });
     
+    const db = mongoose.connection;
+    // db.on('error', function(error){
+    //   console.log(error)
+    //   let errorBody = JSON.stringify({
+    //     status: 'error',
+    //     message: 'Došlo je do problema na našim serverima, pokušajte kasnije!'
+    //   });
+    //   callback(null, {
+    //     statusCode: 500,
+    //     body: errorBody
+    //   })
+    // });
+    db.once('open', function () {
+      console.log('hey');
+      // const userSchema = new mongoose.Schema({
+      //   username: String,
+      //   pass: String
+      // });
+      // const User = new mongoose.model('User', userSchema);
+      // let authenticated = checkCredentials(data.username, data.pass);
+      // let authenticationSuccessBody = JSON.stringify({
+      //   status: 'success',
+      //   message: 'Uspešno ste prijavljeni!'
+      // });
+      // let authenticationErrorBody = JSON.stringify({
+      //   status: 'error',
+      //   message: 'Korisničko ime ili šifra su netačni, pokušajte ponovo!'
+      // });
+      callback(null, {
+        statusCode: 200,
+        // http status code
+        body: authenticated ? authenticationSuccessBody : authenticationErrorBody
+      })
+});
 };
